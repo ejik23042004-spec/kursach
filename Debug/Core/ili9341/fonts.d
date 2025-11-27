911e53f46bb4de512ffec6ade92be790942eada7
Ежицкиц <ejik23042004@gmail.com>
2025-11-27 16:25:19 +0700

    12345

---
+++ Debug/Core/ili9341/fonts.d
@@ -0,0 +1,2 @@
+Core/ili9341/fonts.o: ../Core/ili9341/fonts.c ../Core/ili9341/fonts.h
+../Core/ili9341/fonts.h:
---
+++ Debug/Core/ili9341/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/ili9341/fonts.c \
+../Core/ili9341/ili9341.c \
+../Core/ili9341/ili9341_touch.c 
+
+C_DEPS += \
+./Core/ili9341/fonts.d \
+./Core/ili9341/ili9341.d \
+./Core/ili9341/ili9341_touch.d 
+
+OBJS += \
+./Core/ili9341/fonts.o \
+./Core/ili9341/ili9341.o \
+./Core/ili9341/ili9341_touch.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/ili9341/%.o Core/ili9341/%.su Core/ili9341/%.cyclo: ../Core/ili9341/%.c Core/ili9341/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32L452xx -c -I../Core/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I../Drivers/CMSIS/Device/ST/STM32L4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/ST/touchgfx/framework/include -I../TouchGFX/generated/fonts/include -I../TouchGFX/generated/gui_generated/include -I../TouchGFX/generated/images/include -I../TouchGFX/generated/texts/include -I../TouchGFX/generated/videos/include -I../TouchGFX/gui/include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-ili9341
+
+clean-Core-2f-ili9341:
+	-$(RM) ./Core/ili9341/fonts.cyclo ./Core/ili9341/fonts.d ./Core/ili9341/fonts.o ./Core/ili9341/fonts.su ./Core/ili9341/ili9341.cyclo ./Core/ili9341/ili9341.d ./Core/ili9341/ili9341.o ./Core/ili9341/ili9341.su ./Core/ili9341/ili9341_touch.cyclo ./Core/ili9341/ili9341_touch.d ./Core/ili9341/ili9341_touch.o ./Core/ili9341/ili9341_touch.su
+
+.PHONY: clean-Core-2f-ili9341
+
